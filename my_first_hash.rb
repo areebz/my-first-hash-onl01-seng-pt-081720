cb6a884b6eccd8fe241a435d74b29de2f082f991
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 05:31:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,10 +29,8 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3,
-                  "muskets" => 2,
-                  "gun powder" => 4
                  }
 
-shipping_manifest ["muskets"] = 1 
-shipping_manifest ["gun powder"] = 0 
+shipping_manifest ["muskets"] = 2  
+shipping_manifest ["gun powder"] = 4
 end
