8f4e0ec3ec9efb031b50bdb15c5d9d5ca2a852a6
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 05:24:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,6 +29,7 @@ def adding
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3,
+                  "pearl necklace" => 1 
                   "muskets" => 2,
                   "gun powder" => 4
                  }
